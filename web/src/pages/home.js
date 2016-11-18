2062af5fbcef9d12c5ed7382c3910f260fc49092
aplchian <alex.boquis@gmail.com>
2016-11-18 10:50:11 -0500

    step1. -app -home -about

---
+++ web/src/pages/home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+  render(){
+    return(
+      <div>
+        <h1>Home</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = Home
