51bd40cd65c90be4c7b5c39b980e5cd7b989ad59
aplchian <alex.boquis@gmail.com>
2016-11-18 11:03:35 -0500

    added react-router

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,22 @@
 const React = require('react')
 const Home = require('./pages/home')
 const About = require('./pages/about')
+const {BrowserRouter, Match, Link} = require('react-router')
 
 
 const App = React.createClass({
   render(){
     return(
-      <div>
-        <Home />
-        <About />
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Match exactly pattern="/" component={Home} />
+          <Match exactly pattern="/about" component={About} />
+        </div>
+    </BrowserRouter>
     )
   }
 })
